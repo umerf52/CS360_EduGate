91dcfb67ace98b9be759b6f6d4da490bd41811d5
Saad Khan <saadahmadkhan98@gmail.com>
2019-04-06 17:58:14 +0500

    Login activity done.

--- app/src/main/java/com/apps/edu_gate/LoginActivity.java
+++ app/src/main/java/com/apps/edu_gate/LoginActivity.java
@@ -1,21 +1,120 @@
 package com.apps.edu_gate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+
+    private static final String TAG = "login user";
+    private FirebaseAuth mAuth;
+    private EditText mEmailField;
+    private EditText mPasswordField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Get a support ActionBar corresponding to this toolbar
-        ActionBar ab = getSupportActionBar();
+        mEmailField = findViewById(R.id.fieldEmail3);
+        mPasswordField = findViewById(R.id.fieldPassword3);
 
-        // Enable the Up button
-        ab.setDisplayHomeAsUpEnabled(true);
+        findViewById(R.id.loginbutton).setOnClickListener(this);
 
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void loginuser(String email, String password){
+        Log.d(TAG, "Login:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "Login:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "Login Failed:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressDialog();
+        if (user != null) {
+            Toast.makeText(LoginActivity.this,
+                    "User signed in: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+            Intent myIntent = new Intent(LoginActivity.this, TutorProfile.class);
+            LoginActivity.this.startActivity(myIntent);
+        } else {
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        loginuser(mEmailField.getText().toString(), mPasswordField.getText().toString());
     }
 }
---
+++ app/src/main/java/com/apps/edu_gate/TutorProfile.java
@@ -0,0 +1,13 @@
+package com.apps.edu_gate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TutorProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_profile);
+    }
+}
