fd1bfc3246d2bdb361adf11b8eeb28be1d4e0a26
Saad Khan <saadahmadkhan98@gmail.com>
2019-04-26 13:04:18 +0500

    tutor info updated

--- app/src/main/java/com/apps/edu_gate/Tutorinfo.java
+++ app/src/main/java/com/apps/edu_gate/Tutorinfo.java
@@ -1,10 +1,16 @@
 package com.apps.edu_gate;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 public class Tutorinfo implements Serializable {
 
-    public String Name, Address, Institution;
+    public String address, cnicNo, contactNo, emailAddress, firstName, gender, lastName, recentInstitution, tutionLocation, urlImage;
+    public int profileStatus;
+    public HashMap<String, String> grade;
+    public HashMap<String, String> subject;
+    public HashMap<String, Double> rating;
+
 
     public Tutorinfo() {
     }
