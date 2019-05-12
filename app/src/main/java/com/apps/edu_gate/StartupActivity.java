3f10812ca7e16d332bf2cc19fad55d70171b94f7
Umer <umer.farooq.042@gmail.com>
2019-05-12 12:53:48 +0500

    Added splash screen

--- app/src/main/java/com/apps/edu_gate/StartupActivity.java
+++ app/src/main/java/com/apps/edu_gate/StartupActivity.java
@@ -12,6 +12,7 @@ public class StartupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         ActionBar ab = getSupportActionBar();
         if (ab != null) {
